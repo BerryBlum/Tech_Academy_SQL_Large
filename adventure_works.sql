04d0faf5c63f090e61c2cf0bb7331a8bd320bb87
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-10-22 15:24:34 -0700

    Update adventure_works.sql
    Updated to include the custom precedure for finding address vs city and pay rate. No mal-intent here nope never, don't know what you're talking about.

--- adventure_works.sql
+++ adventure_works.sql
@@ -43,3 +43,61 @@
 --EXEC dbo.uspGetAddressCount 'Calgary', @AddressCount OUTPUT
 --SELECT @AddressCount
 
+--CREATE PROCEDURE dbo.uspTryCatchTest
+--AS
+--BEGIN TRY
+--	SELECT 1/0
+--END TRY
+--BEGIN CATCH
+--	SELECT ERROR_NUMBER() AS ErrorNumber,
+--	ERROR_SEVERITY() AS ErrorSeverity,
+--	ERROR_STATE() AS ErrorState,
+--	ERROR_PROCEDURE() AS ErrorProcedure,
+--	ERROR_LINE() AS ErrorLine,
+--	ERROR_MESSAGE() AS ErrorMessage;
+--END CATCH
+
+--dbo.uspTryCatchTest --usp for USER STORED PROCEDURE
+--KEEP NAMING SIMPLE AND MEANINGFUL
+
+--SCHEMA.PREFIX(ACTION/OBJECT) NAMING CONVENTION
+
+--CREATE PROC dbo.uspGetAddressCity @City nvarchar(30)
+--AS
+--SELECT * FROM Person.Address
+--WHERE City = @City
+--GO
+
+--CREATE PROC dbo.uspGetAddressCity @City nvarchar(30)  --START OF ALTER COMMANDS
+--AS
+--SET NOCOUNT ON
+--SELECT * FROM Person.Address
+--WHERE City = @City
+--PRINT @@ROWCOUNT
+--GO
+
+--DROP PROC dbo.uspGetAddressCity
+--GO
+
+--ALTER PROC dbo.uspGetAddressCity @City nvarchar(30)
+--AS
+--SET NOCOUNT OFF
+--SELECT * FROM Person.Address
+--WHERE City LIKE @City + '%'
+--PRINT @@ROWCOUNT
+--GO
+
+--dbo.uspGetAddressCity @City = 'Calgary'
+
+ALTER PROC dbo.uspPersonalInformation @City nvarchar(30), @Rate int
+AS
+SELECT a1.City, a1.AddressLine1, a2.Rate, a3.HireDate
+FROM Person.Address a1
+INNER JOIN HumanResources.Employee a3 ON a3.BusinessEntityID = a1.AddressID
+INNER JOIN HumanResources.EmployeePayHistory a2 ON a2.BusinessEntityID = a3.BusinessEntityID
+WHERE a1.City = @City AND a2.Rate = @Rate
+GO
+
+dbo.uspPersonalInformation @City = 'Seattle', @Rate = 25 --DEFINITELY NOT FOR NEFARIOUS DEEDS...
+
+--Definitely referenced https://www.sqldatadictionary.com/AdventureWorks2014.pdf --
